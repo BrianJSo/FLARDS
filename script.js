9d869e333b22a704dc702e11b5953f561cb61716
Brian So <brianjezreelso@gmail.com>
2026-08-07 21:09:09 +0800

    answering system

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+function loadApp(){
+  const csv = document.querySelector(".csv .input");
+  csv.value = localStorage.getItem("csv");
+
+  localStorage.setItem("itemNum", 0);
+  localStorage.setItem("score", 0);
+}
+
 function loadCSV(){
   const answerDiv = document.querySelector(".answer");
   const csvDiv = document.querySelector(".csv");
@@ -8,5 +16,37 @@ function loadCSV(){
 
   localStorage.setItem("csv", csv);
   
-  alert(localStorage.getItem("csv"));
+  nextItem();
+}
+
+function nextItem() {
+  const itemNum =  localStorage.getItem("itemNum");
+  const csv = document.querySelector(".csv .input").value;
+  const maintext = document.querySelector(".div1 .maintext");
+  const subtext = document.querySelector(".div1 .subtext");
+
+  const list = csv.split("\n");
+
+  const main = list[itemNum].split(",");
+  maintext.innerHTML = main[0];
+  subtext.innerHTML = main[1];
+  
+  localStorage.setItem("answerKey", main[2]);
+}
+
+function answer(){
+  const itemNum =  parseInt(localStorage.getItem("itemNum"));
+  const answerKey = localStorage.getItem("answerKey");
+  const answer = document.querySelector(".answer .input").value;
+
+  if (answerKey == answer){
+    const curScore = parseInt(localStorage.getItem("score"));
+    localStorage.setItem("score", curScore+1);
+  }
+  else {
+    console.log("wrong");
+  }
+  
+  localStorage.setItem("itemNum", itemNum+1);
+  nextItem()
 }

0c2dcdb89421bcc61d6c1dc9dc69e5046678eaaa
Brian So <brianjezreelso@gmail.com>
2026-08-08 19:04:19 +0800

    randomizer

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+let list = [];
+let answerKey;
+let score;
+
 function loadApp(){
   const csv = document.querySelector(".csv .input");
   csv.value = localStorage.getItem("csv");
-
-  localStorage.setItem("itemNum", 0);
-  localStorage.setItem("score", 0);
 }
 
 function loadCSV(){
@@ -11,42 +12,38 @@ function loadCSV(){
   const csvDiv = document.querySelector(".csv");
   const csv = document.querySelector(".csv .input").value;
 
+  localStorage.setItem("csv", csv);
+
+  list = csv.split("\n");
+  score = 0;
+
   answerDiv.classList.toggle("hide");
   csvDiv.classList.toggle("hide");
 
-  localStorage.setItem("csv", csv);
-  
   nextItem();
 }
 
 function nextItem() {
-  const itemNum =  localStorage.getItem("itemNum");
-  const csv = document.querySelector(".csv .input").value;
   const maintext = document.querySelector(".div1 .maintext");
   const subtext = document.querySelector(".div1 .subtext");
 
-  const list = csv.split("\n");
-
-  const main = list[itemNum].split(",");
-  maintext.innerHTML = main[0];
-  subtext.innerHTML = main[1];
+  const itemNum = Math.floor(Math.random() * list.length);
   
-  localStorage.setItem("answerKey", main[2]);
+  const item = list.splice(itemNum,1)[0];
+  const itemArr = item.split(",");
+  maintext.innerHTML  = itemArr[0];
+  subtext.innerHTML   = itemArr[1];
+  answerKey           = itemArr[2];
 }
 
 function answer(){
-  const itemNum =  parseInt(localStorage.getItem("itemNum"));
-  const answerKey = localStorage.getItem("answerKey");
   const answer = document.querySelector(".answer .input").value;
 
   if (answerKey == answer){
-    const curScore = parseInt(localStorage.getItem("score"));
-    localStorage.setItem("score", curScore+1);
+    score++;
   }
   else {
     console.log("wrong");
   }
-  
-  localStorage.setItem("itemNum", itemNum+1);
   nextItem()
 }
